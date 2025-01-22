7a062d25a7845a358f58952448027f9a9a496725
erenkurtt <kurt.eren@togg.com.tr>
2025-01-22 13:59:24 +0300

    production build fix

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { AntdRegistry } from '@ant-design/nextjs-registry';
-import StoreProvider from "@/app/src/store/Provider";
+import StoreProvider from "./src/store/Provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- ui/app/src/pages/SpinPage.tsx
+++ ui/app/src/pages/SpinPage.tsx
@@ -2,12 +2,15 @@
 import React from "react";
 import Slot from "../components/slotspin/Slot";
 import Navigation from "../components/navigation/Navigation";
+import { Suspense } from 'react';
 
 const SpinPage: React.FC = () => {
     return (
         <div>
-            <Navigation />
-            <Slot />
+            <Suspense fallback={<div>Loading...</div>}>
+                <Navigation />
+                <Slot />
+            </Suspense>
         </div>
     )
 };
